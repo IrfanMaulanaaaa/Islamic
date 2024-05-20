41a334b3272419eaf670494b3183a87feb563f25
Irfan Maulana <121864328+IrfanMaulanaaaa@users.noreply.github.com>
2024-05-20 18:30:49 +0800

    first commit

---
+++ php_config/config.php
@@ -0,0 +1,13 @@
+<?php
+// Konfigurasi database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "islamic";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
